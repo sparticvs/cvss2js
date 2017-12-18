a7f2a02841cda5a65108d96c022a4a5cd9587061
sparticvs <sparticvs@popebp.com>
2017-12-18 18:24:23 -0500

    Add #getScore tests

--- test/main.js
+++ test/main.js
@@ -4,6 +4,21 @@ const assert = require('assert');
 
 describe('CVSS', function() {
     describe('#getScore()', function() {
+        it('should return 6.8 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P"', function() {
+            assert.equal(CVSS.getScore("AV:N/AC:M/Au:N/C:P/I:P/A:P"), 6.8);
+        });
+
+        it('should return 5.2 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C"', function() {
+            assert.equal(CVSS.getScore("AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C"), 5.2);
+        });
+
+        it('should return 1.6 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C/CDP:LM/TD:L/CR:M/IR:M/AR:L"', function() {
+            assert.equal(CVSS.getScore("AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C/CDP:LM/TD:L/CR:M/IR:M/AR:L"), 1.6);
+        });
+
+        it('should throw an exception when given "AV:N/Au:N/A:P"', function() {
+            assert.throws(() => { CVSS.getScore("AV:N/Au:N/A:P") }, Error);
+        });
     });
 
     describe('#getBaseScore()', function() {
