de93f55de05d65071c7a04e45d05030fbf60e04c
sparticvs <sparticvs@popebp.com>
2017-12-18 19:27:58 -0500

    Implement remaining functions

--- test/main.js
+++ test/main.js
@@ -22,12 +22,45 @@ describe('CVSS', function() {
     });
 
     describe('#getBaseScore()', function() {
+        it('should return 6.8 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P"', function() {
+            assert.equal(CVSS.getBaseScore("AV:N/AC:M/Au:N/C:P/I:P/A:P"), 6.8);
+        });
+
+        it('should return 6.8 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C"', function() {
+            assert.equal(CVSS.getBaseScore("AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C"), 6.8);
+        });
+
+        it('should return 6.8 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C/CDP:LM/TD:L/CR:M/IR:M/AR:L"', function() {
+            assert.equal(CVSS.getBaseScore("AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C/CDP:LM/TD:L/CR:M/IR:M/AR:L"), 6.8);
+        });
     });
 
-    describe('#getTemporalScore()', function(){ 
+    describe('#getTemporalScore()', function() { 
+        it('should return 6.8 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P"', function() {
+            assert.equal(CVSS.getTemporalScore("AV:N/AC:M/Au:N/C:P/I:P/A:P"), 6.8);
+        });
+
+        it('should return 5.2 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C"', function() {
+            assert.equal(CVSS.getTemporalScore("AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C"), 5.2);
+        });
+
+        it('should return 5.2 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C/CDP:LM/TD:L/CR:M/IR:M/AR:L"', function() {
+            assert.equal(CVSS.getTemporalScore("AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C/CDP:LM/TD:L/CR:M/IR:M/AR:L"), 5.2);
+        });
     });
 
     describe('#getEnvironmentalScore()', function() {
+        it('should return 6.8 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P"', function() {
+            assert.equal(CVSS.getEnvironmentalScore("AV:N/AC:M/Au:N/C:P/I:P/A:P"), 6.8);
+        });
+
+        it('should return 5.2 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C"', function() {
+            assert.equal(CVSS.getEnvironmentalScore("AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C"), 5.2);
+        });
+
+        it('should return 1.6 when given "AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C/CDP:LM/TD:L/CR:M/IR:M/AR:L"', function() {
+            assert.equal(CVSS.getEnvironmentalScore("AV:N/AC:M/Au:N/C:P/I:P/A:P/E:U/RL:TF/RC:C/CDP:LM/TD:L/CR:M/IR:M/AR:L"), 1.6);
+        });
     });
 
     describe('#getRating()', function() {
@@ -35,8 +68,24 @@ describe('CVSS', function() {
             assert.equal(CVSS.getRating(), undefined);
         });
 
-        it('should return "Critical" when score is 10', function() {
-            assert.equal(CVSS.getRating(10), "Critical");
+        it('should return "Informational" when score is 0', function() {
+            assert.equal(CVSS.getRating(0), "Informational");
+        });
+
+        it('should return "Low" when score is 0.1', function() {
+            assert.equal(CVSS.getRating(0.1), "Low");
+        });
+
+        it('should return "Medium" when score is 4', function() {
+            assert.equal(CVSS.getRating(4), "Medium");
+        });
+
+        it('should return "High" when score is 7', function() {
+            assert.equal(CVSS.getRating(7), "High");
+        });
+
+        it('should return "Critical" when score is 9', function() {
+            assert.equal(CVSS.getRating(9), "Critical");
         });
     });
 
